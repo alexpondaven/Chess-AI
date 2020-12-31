9cddee382087902a16acc375446642f4e4baf4d4
alexpondaven <alex.pondaven@gmail.com>
2020-12-31 19:52:59 +0000

    fixed board and added central origin for pieces

--- chess_ai/main.cpp
+++ chess_ai/main.cpp
@@ -15,7 +15,7 @@ int aboard[8][8] = {{-5,-4,-3,-2,-1,-3,-4,-5},
                     { 6, 6, 6, 6, 6, 6, 6, 6},
                     { 5, 4, 3, 2, 1, 3, 4, 5} };
 
-void loadPos(int w, int h, int sw, int sh) {
+void loadPos(float sw, float sh, float psize, float scaled) {
     int count = 0;
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
@@ -24,31 +24,44 @@ void loadPos(int w, int h, int sw, int sh) {
             int row = abs(n) - 1;
             int col = n<0;
             p[count].setTextureRect(IntRect(sw * row, sh * col, sw, sh));
-            p[count].setScale(((float)w / 8 - 4) / (float)sw, ((float)h / 8 - 4) / (float)sh);
-            p[count].setPosition(((float)w / 8) * j + 2, ((float)h / 8) * i + 2);
+            p[count].setOrigin(sw / 2, sh / 2);
+            p[count].setScale( (psize / sw) * scaled, (psize / sh) * scaled);
+            p[count].setPosition(psize * (j + 0.5), psize * (i + 0.5));
             count++;
         }
     }
 }
 
+RectangleShape boundingBox(Sprite s) {
+    // draw rectangle around sprite
+    RectangleShape rect;
+    rect.setSize(Vector2f(s.getGlobalBounds().width, s.getGlobalBounds().height));
+    rect.setFillColor(Color::Transparent);
+    rect.setOutlineColor(Color::Red);
+    rect.setOutlineThickness(1);
+    rect.setPosition(s.getPosition() - rect.getSize() / 2.0f);
+    return rect;
+}
+
 int main()
 {
-    // setting up window
+    // parameters
     int width = 512;
     int height = 512;
-    int psize = width / 8; // piece size
+    float psize = (float)width / 8.0f; // piece size
+    float scaled = 0.9; // how much to scale down the pieces
+
+    // render window
     RenderWindow window(VideoMode(width, height), "Chess", Style::Close | Style::Resize);
  
     // Adding images and sprites
     Texture pieces, boardi;
     pieces.loadFromFile("images/pieces2.png");
-    boardi.loadFromFile("images/board.png");
+    boardi.loadFromFile("images/board1.png");
 
-    Sprite piece(pieces);
-    Sprite board(boardi);
-    
     // scale board size to fit window size
     Vector2u boardSize = boardi.getSize();
+    Sprite board(boardi);
     board.setScale(width / (float)boardSize.x, height / (float)boardSize.y);
 
     // scale sprites
@@ -58,7 +71,7 @@ int main()
     // pass in window width/height and sprite width/height
     //pieces.setSmooth(true); // can't tell if this helps
     for (int i=0;i<32;i++) p[i].setTexture(pieces);
-    loadPos(width, height, sw, sh);
+    loadPos((float)sw, (float)sh, psize, scaled);
     
    
 
@@ -68,7 +81,9 @@ int main()
 
     while (window.isOpen())
     {
-        Vector2i mousePos = Mouse::getPosition(window);
+        Vector2i mouse = Mouse::getPosition(window);
+        Vector2i mousePos = Vector2i(window.mapPixelToCoords(mouse)); // fixes mouse position when rescaling window
+
         Event e;
         while (window.pollEvent(e))
         {
@@ -94,8 +109,9 @@ int main()
             if (e.type == Event::MouseButtonReleased && e.key.code == Mouse::Left) {
                 moving = 0;
                 // position of center of piece
-                Vector2f pos = p[pmoving].getPosition() + Vector2f(psize / 2, psize / 2);
-                Vector2f nPos = Vector2f(int(pos.x / psize) * psize + 2, int(pos.y / psize) * psize + 2);
+                
+                Vector2f pos = p[pmoving].getPosition();
+                Vector2f nPos = Vector2f(float(int(pos.x / psize)+0.5) * psize, float(int(pos.y / psize)+0.5) * psize);
                 p[pmoving].setPosition(nPos);
             }
         }
@@ -103,14 +119,22 @@ int main()
         if (moving) {
             // use offx or offy to drag and keep relative position of cursor to piece
             //piece.setPosition(mousePos.x-offx, mousePos.y-offy);
-            p[pmoving].setPosition(mousePos.x - p[pmoving].getGlobalBounds().width / 2, mousePos.y - p[pmoving].getGlobalBounds().height / 2);
+            p[pmoving].setPosition(Vector2f(mousePos));
 
         }
+        // circle at cursor
+        //CircleShape c(10);
+        //c.setPosition(Vector2f(mousePos)-Vector2f(5,5));
 
+        
         // draw
         window.clear();
         window.draw(board);
-        for (int i = 0; i < 32; i++) window.draw(p[i]);
+        for (int i = 0; i < 32; i++) {
+            window.draw(p[i]);
+            window.draw(boundingBox(p[i])); // draw bounding box around pieces
+        }
+        //window.draw(c);
         window.display();
     }
 
